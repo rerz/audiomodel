02862cdc3d9cb049aae917416ade7fdaca473f5d
rerz <5412228+rerz@users.noreply.github.com>
2024-08-29 11:39:16 +0200

    add custom burn dep

--- src/bin/thing.rs
+++ src/bin/thing.rs
@@ -12,45 +12,36 @@ type B = Autodiff<Wgpu>;
 fn main() {
     let mut resampled_dataset = SqliteDatasetWriter::new("music_genres.sqlite", false).unwrap();
 
-    HuggingfaceDatasetLoader::new("lewtun/music_genres")
+    let train_samples = HuggingfaceDatasetLoader::new("lewtun/music_genres")
         .dataset::<MusicGenresItem>("train")
         .unwrap()
         .iter()
         .par_bridge()
         .map(|track| track.into())
-        .for_each(|item: AudioSample| {
-            resampled_dataset
-                .write(
-                    "train",
-                    &AudioSample {
+        .map(|item: AudioSample| AudioSample {
                         audio: resample(item.audio, item.sr, 16_000),
                         sr: 16_000,
                         id: item.id,
                         group_label: item.group_label,
-                    },
-                )
-                .unwrap();
-        });
+                    })
+        .collect::<Vec<_>>();
 
-    HuggingfaceDatasetLoader::new("lewtun/music_genres")
+    resampled_dataset.write_all("train", &train_samples).unwrap();
+
+    let test_samples = HuggingfaceDatasetLoader::new("lewtun/music_genres")
         .dataset::<MusicGenresItem>("test")
         .unwrap()
         .iter()
         .par_bridge()
         .map(|track| track.into())
-        .for_each(|item: AudioSample| {
-            resampled_dataset
-                .write(
-                    "test",
-                    &AudioSample {
+        .map(|item: AudioSample| AudioSample {
                         audio: resample(item.audio, item.sr, 16_000),
                         sr: 16_000,
                         id: item.id,
                         group_label: item.group_label,
-                    },
-                )
-                .unwrap();
-        });
+                    }).collect::<Vec<_>>();
+
+    resampled_dataset.write_all("test", &test_samples).unwrap();
 
     resampled_dataset.set_completed().unwrap();
 }
